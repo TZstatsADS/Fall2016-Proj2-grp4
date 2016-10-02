e796b9c9f7459a66baa10f65de220c9fe4087d50
PengfeiWangWZ <hehtihawa@gmail.com>
2016-10-02 10:04:53 -0400

    connect
    run python code in R

---
+++ lib/rPython.R
@@ -0,0 +1,12 @@
+library(rPython)
+python.exec('import sys')
+python.exec("sys.path.append('/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages')")
+# python.assign("plate_number","4222GE")
+python.load('Desktop/prj2/violation_check.py')
+python.call('Ticket_check',"4222GE")
+
+server = function(input, output, session){
+  output$out1 <- renderPrint({
+    python.call('Ticket_check',input$plate_number)
+  })
+}
