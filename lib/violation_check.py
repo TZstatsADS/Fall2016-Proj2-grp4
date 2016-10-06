227184e361e71174a926e33daf2545ac26583b78
PengfeiWangWZ <hehtihawa@gmail.com>
2016-10-06 01:04:20 -0400

    part-3
    scratch violation update

--- lib/violation_check.py
+++ lib/violation_check.py
@@ -1,5 +1,6 @@
 import selenium
 from selenium import webdriver
+import pandas as pd
 
 def Ticket_check(plate_number):
     driver_path = '/Users/pengfeiwang/Downloads/ots/chromedriver'
@@ -12,11 +13,27 @@ def Ticket_check(plate_number):
         pass
     driver.find_element_by_xpath('//div/p/a[@class="arrow-link small link-bold link-arrow-blue"]').click()
     driver.find_element_by_css_selector('input[name=\"args.PLATE\"]').send_keys(plate_number)
+    driver.find_element_by_id('search_next').click()
     try:
-        driver.find_element_by_id('search_next').click()
         summary = driver.find_element_by_class_name('results-summary-label').text
-        money_owned = driver.find_element_by_class_name('total-amount-owed-value').text
-        print summary,money_owned
+#         money_owned = driver.find_element_by_class_name('total-amount-owed-value').text
+#         print summary,money_owned
+        col_length = len(driver.find_elements_by_class_name('expander-icon'))
+        for i in range(col_length):
+            driver.find_elements_by_class_name('expander-icon')[i].click()
+        item_length = len(driver.find_elements_by_class_name('violation-title'))
+        violation_df = pd.DataFrame(columns = ['number','des','value','time'])
+        for i in range(item_length):
+            violation_number = dict()
+            violation_number['number'] = driver.find_elements_by_xpath('//div/ul/li/span[contains(text(),"Violation #: ")]/following-sibling::span[@class="violation-details-single-value1"]')[i].text
+            violation_number['des'] = driver.find_elements_by_class_name('violation-title')[i].text.split(':')[0]
+            violation_number['value'] = driver.find_elements_by_class_name('violation-value')[i].text
+            violation_number['time'] = driver.find_elements_by_xpath('//div/ul/li/span[contains(text(),"/")]')[i].text
+            violation_df = violation_df.append(violation_number,ignore_index=True)
+        driver.close()
+        return(violation_df)
+    
     except:
-        print "The plate number was not found. Please verify the plate number, state and type values and search again. Please note if your ticket was recently issued or if you have no outstanding violations, your plate information will not be found. If you wish to pay a ticket you just received, search by the ticket number to ensure it has not been entered into the system. "
-    driver.close()
+        driver.close()
+        return("The plate number was not found. Please verify the plate number, state and type values and search again. Please note if your ticket was recently issued or if you have no outstanding violations, your plate information will not be found. If you wish to pay a ticket you just received, search by the ticket number to ensure it has not been entered into the system. ")
+    
