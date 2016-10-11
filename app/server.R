7ed00f19f0a91972f9bc6deee92e4d3a088c4f1e
Tian Sheng <ts2949@columbia.edu>
2016-10-11 16:23:00 -0400

    update of the ui and server

--- app/server.R
+++ app/server.R
@@ -13,9 +13,102 @@ business<-read.xlsx("business_parking_lot.xlsx")
 
 shinyServer(function(input,output) {
   nyc<-reactive({
-    nyc_map<-leaflet()%>%setView(lng=-73.90,lat=40.75,zoom=11)%>%addTiles()
+    state_popup <- paste0("<strong>Estado: </strong>", 
+                          dat.pv$Street.Name, 
+                          "<br><strong>PIB per c?pita, miles de pesos, 2008: </strong>", 
+                          dat.pv$Plate.ID)
+    
+    nyc_map<-leaflet(data = dat.pv) %>%
+      setView(lat= 40.7589, lng = -73.9851, zoom = 12)%>%
+      #addProviderTiles("CartoDB.Positron") %>%
+      addProviderTiles("Stamen.Toner") %>%
+      addProviderTiles("Stamen.TonerLabels") %>%
+      addMarkers(lng = as.numeric(dat.pv$lng),
+                 lat = as.numeric(dat.pv$lat),
+                 popup = state_popup,
+                 clusterOptions = markerClusterOptions())
+    
     return(nyc_map)
   })
+  
+  violation<-reactive({
+    GetViocodeData <- function(violation.code) {
+      dat.pv.viocode <- dat.pv %>% filter(Violation.Code == violation.code) %>% # Switch code input 
+        mutate(lng = substr(lng, 1, 7), lat = substr(lat, 1, 6), coor = paste0(lng, ",", lat)) %>%
+        group_by(coor) %>%
+        summarise(sum = n()) %>%
+        mutate(lng = as.numeric(substr(coor, 1, 7)), lat = as.numeric(substr(coor, 9, 14))) %>%
+        filter(!is.na(lat))
+      return(dat.pv.viocode)
+    }
+    # Color List
+    color.list <- c("#edc951", "#eb6841", "#cc2a36", "#4f372d", "#00a0b0")
+    # Violation type switch #
+    number<-switch(input$type,
+                   "Park Near Stores"= 36,
+                   "Park Without Current Inspection Sticker"=71,
+                   "Double Parking"=46,
+                   "Parking Time Exceeds"=37,
+                   "Park Near Fire Hydrant"=40)
+    # Color switch #
+    color_s<-switch(input$type,
+                    "Park Near Stores"= "#edc951",
+                    "Park Without Current Inspection Sticker"="#eb6841",
+                    "Double Parking"="#cc2a36",
+                    "Parking Time Exceeds"="#4f372d",
+                    "Park Near Fire Hydrant"="#00a0b0")
+    # Make data frames 
+    dat.vio40 <- GetViocodeData(40)
+    dat.vio36 <- GetViocodeData(36)
+    dat.vio71 <- GetViocodeData(71)
+    dat.vio46 <- GetViocodeData(46)
+    dat.vio37 <- GetViocodeData(37)
+    violation_map<-leaflet(data = GetViocodeData(number)) %>% #Data can be changed
+      setView(lat= 40.7589, lng = -73.9851, zoom = 12)%>%
+      addProviderTiles("CartoDB.Positron") %>%
+      addProviderTiles("Stamen.Toner") %>%
+      addProviderTiles("Stamen.TonerLabels") %>% 
+      addCircleMarkers(lng = ~lng,
+                       lat = ~lat,
+                       radius = 3,
+                       color = "#edc951", # Color can be changed 
+                       #radius = ~sum, 
+                       stroke= FALSE,
+                       fillOpacity = 0.8)
+    return(violation_map)
+  })
+  
+  #### output of nyc_map #####
+  output$nyc_map<-renderLeaflet({state_popup <- paste0("<strong>Estado: </strong>", 
+                                                       dat.pv$Street.Name, 
+                                                       "<br><strong>PIB per c?pita, miles de pesos, 2008: </strong>", 
+                                                       dat.pv$Plate.ID)
+  
+  leaflet(data = dat.pv) %>%
+    addProviderTiles("CartoDB.Positron") %>%
+    addMarkers(lng = as.numeric(dat.pv$lng),
+               lat = as.numeric(dat.pv$lat),
+               popup = state_popup,
+               clusterOptions = markerClusterOptions())
+  nyc()
+  })
+  #### output of violation_map ####
+  output$violation_map<-renderLeaflet(
+    violation_map1<-leaflet(data = GetViocodeData(number)) %>% #Data can be changed
+      setView(lat= 40.7589, lng = -73.9851, zoom = 12)%>%
+      addProviderTiles("CartoDB.Positron") %>%
+      addProviderTiles("Stamen.Toner") %>%
+      addProviderTiles("Stamen.TonerLabels") %>% 
+      addCircleMarkers(lng = ~lng,
+                       lat = ~lat,
+                       radius = 3,
+                       color = "#edc951", # Color can be changed 
+                       #radius = ~sum, 
+                       stroke= FALSE,
+                       fillOpacity = 0.8),
+    return(violation_map1)
+  )
+  
   block<-reactive({
     # Add color columns
     rule_simp2cha=lapply(res1$rule_simplified,as.character,stringsAsFactors=FALSE)
@@ -77,9 +170,8 @@ shinyServer(function(input,output) {
     alist=as.data.frame(alist)
     return(alist)
   })
-  output$nyc_map<-renderLeaflet({
-    nyc()
-  })
+  
+  #### output of blockmap ####
   output$block_map<-renderLeaflet({
     if(input$show=="Government Office"){
       block()%>%addMarkers(-73.941098,40.800833,icon=policeIcon())%>%
@@ -95,3 +187,35 @@ shinyServer(function(input,output) {
     number()
   })
 })
+
+output$ggBarPlotA<-renderPlot({
+  mon<-switch(input$date1,
+              "Jan"=1,
+              "Feb"=2,
+              "Mar"=3,
+              "Apr"=4,
+              "May"=5,
+              "Jun"=6,
+              "Jul"=7,
+              "Aug"=8,
+              "Sept"=9,
+              "Oct"=10,
+              "Nov"=11,
+              "Dec"=12)
+  all<-read.rds("timeline.rds")
+  c = filter(all, month==mon)
+  barplot<-ggplot(c, aes(f,col=e)) + 
+    geom_histogram(binwidth=1,position="identity")+geom_freqpoly(binwidth=1)+ 
+    labs(title = "Timeline of Violation distribution") +
+    theme(axis.text = element_text(size = 14),
+          legend.key = element_rect(fill = "white"),
+          legend.background = element_rect(fill = "grey40"),
+          legend.position = c(0.14, 0.80),
+          panel.grid.major = element_line(colour = "grey40"),
+          panel.grid.minor = element_blank(),
+          panel.background = element_rect(fill = "black"),
+          plot.background = element_rect(fill = "black",colour = "black",size = 2,
+                                         linetype = "longdash"))
+  return(barplot)
+})
+
--- app/ui.R
+++ app/ui.R
@@ -1,28 +1,31 @@
 library(shiny)
+library(maps)
+library(mapproj) 
 library(leaflet)
 library(rPython)
 library(rgdal)
 library(magrittr)
+library(ggplot2)
 library(sp)
 library(jsonlite)
 library(openxlsx)
 library(googleway)
 
-shinyUI(navbarPage("Better Parking",theme="bootstrap_theme_02.css",
+# Define UI for application that draws 4 pages
+shinyUI(navbarPage(id="navbar",title="Parkman Go",
+                   theme="black.css",
                    tabPanel("Introduction",
-                     titlePanel(h2("Introduction"))
-                   ),
+                            titlePanel(h2("Introduction")),
+                            mainPanel(tabPanel("Introduction",includeMarkdown("introduction.md")))),
                    tabPanel("NYC Map",
-                     titlePanel(h2("Parking Violation Analysis")),
-                     sidebarLayout(
-                       sidebarPanel(
-                         
-                       ),
-                       mainPanel(
-                         leafletOutput("nyc_map",height=500)
-                       )
-                     )
-                   ),
+                            div(class="outer"),
+                            leafletOutput("nyc_map",height=500),
+                            leafletOutput("violation_map",height=500),
+                            absolutePanel(id="controls",class="panel panel-default",
+                                          fixed = TRUE, draggable = TRUE, top = 60, left = "auto",
+                                          right = 20, bottom = "auto", width = 330, height = "auto",h4("Violation Type"),
+                            selectInput("type",label = "Choose a violation type to display",choices = c("Park Near Stores","Park Without Current Inspection Sticker",
+                           "Double Parking","Parking Time Exceeds","Park Near Fire Hydrant"),selected = "Park Near Stores"))),
                    tabPanel("Block Map",
                      titlePanel(h2("Parking Area Analysis")),
                      sidebarLayout(
@@ -60,16 +63,27 @@ shinyUI(navbarPage("Better Parking",theme="bootstrap_theme_02.css",
                      )
                    ),
                    tabPanel("Statistics",
-                     titlePanel(h2("Data Analysis")),
-                     sidebarLayout(
-                       sidebarPanel(
-                         
-                       ),
-                       mainPanel(
-                         
-                       )
-                     )
-                   ),
+                            titlePanel(h2("Data Analysis")),
+                            sidebarLayout(position="right",
+                                          sidebarPanel(
+                                            conditionalPanel(condition="input.ccpanel==1",
+                                                             helpText("First graph:bar chart for Timeline distribution."),
+                                                             helpText("Second graph: Pie chart for categories based on the Top 10 with the most counts."),
+                                                             helpText("Third graph: Histogram for violation fee comparation.") ),
+                                            selectInput("date1", 
+                                                        label = "Choose a month to display",
+                                                        choices = c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"),
+                                                        selected="Jan"
+                                            )), 
+                                          mainPanel(tabsetPanel(
+                                            tabPanel("Barchart",br(),tags$div(class="descrip_text", 
+                                                                              textOutput("Bar_text")), br(),
+                                                     plotOutput("ggBarPlotA",height="600px"),value=1),
+
+                                            tabPanel("Piechart",br(),tags$div(class="descrip_text",
+                                                                              textOutput("Pie_text")), br(),
+                                                     plotlyOutput("ggPiePlot",height="600px"),value=1)
+                                          )))),
                    tabPanel("Reference and Contact",
                      navlistPanel("Reference and Contact",
                                   tabPanel("Reference"
